735b07e9515237b3df51075cd0463ea252a7e415
wangz <outlook.com>
2025-03-29 16:33:15 +0800

    tijiao

--- 2025/text_03_28_0.cpp
+++ 2025/text_03_28_0.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <windows.h>
+#include <assert.h>
+namespace student_Validation_Limitations
+{
+    uint8_t age_limitation = 150;
+
+    uint8_t tel_long = 11;
+
+    uint8_t address_long = 18;
+
+    template <class grades_type>
+    class grades
+    {
+        public:
+        grades_type grades_main = (grades_type)150;
+
+        grades_type grades_min_s = (grades_type)50;
+
+        grades_type grades_main_s = (grades_type)70;
+    };
+}
 template <class grades_type>
 class Information_management
 {
@@ -70,9 +91,226 @@ class Information_management
         student* data;
         student_list* prev;
         student_list* next;
+
+        class student_Grade_checks
+        {
+            public:
+            typedef struct input
+            {
+                std::string informationcheck_name;
+                void* address;
+                void* Limit_parameters;
+                BOOL(*Enteralimit)(const void*, const void*);
+            }inputs;
+
+            static inline uint8_t Enteralimit_student_long_compare(const uint64_t* data)
+            {
+                uint8_t long_compare = 0;
+                uint64_t datas = *data;
+                while ( datas > 0 )
+                {
+                    long_compare++;
+                    datas /= 10;
+                }
+                return long_compare;
+                
+            }
+            static BOOL Enteralimit_student_age(const void* data, const void* Limit_parameters)
+            {
+                if (Limit_parameters)
+                {
+                    if ((*(uint8_t*)data) <= *(uint8_t*)Limit_parameters)
+                    {
+                        return TRUE;
+                    }
+                    else
+                    {
+                        std::cout << "错误!,年龄不能大于" << *(uint8_t*)Limit_parameters << std::endl;
+                        return FALSE;
+                    }
+                }
+                else
+                {
+                    if (*(uint8_t*)data == 0)
+                    {
+                        std::cout << "错误!,年龄不能为0" << std::endl;
+                        return FALSE;
+                    }
+                    else
+                    {
+                        return TRUE;
+                    }
+                }
+            }
+            static BOOL Enteralimit_student_tel(const void* data, const void* Limit_parameters)
+            {
+                uint64_t tel = *(uint64_t*)data;
+                if(Limit_parameters)
+                {
+                    if(student_Grade_checks::Enteralimit_student_long_compare(&tel) != *(uint8_t*)Limit_parameters)
+                    {
+                        std::cout << "错误!,电话号码长度应为" << *(uint8_t*)Limit_parameters << std::endl;
+                        std::cout << "当前电话号码长度为"  << student_Grade_checks::Enteralimit_student_long_compare(&tel) << std::endl;
+                        return FALSE;
+                    }
+                    return TRUE;
+                }
+                else
+                {
+                    if(tel > 0)
+                    {
+                        return TRUE;
+                    }
+                    return FALSE;
+                }
+            }
+            static BOOL Enteralimit_student_id(const void* data, const void* Limit_parameters)
+            {
+                uint64_t id = *(uint64_t*)data;
+                if(Limit_parameters)
+                {
+                    if(student_Grade_checks::Enteralimit_student_long_compare(&id) != *(uint8_t*)Limit_parameters)
+                    {
+                        std::cout << "错误!,身份证号码长度应为" << *(uint8_t*)Limit_parameters << std::endl;
+                        std::cout << "当前身份证号码长度为"  << student_Grade_checks::Enteralimit_student_long_compare(&id) << std::endl;
+                        return FALSE;
+                    }
+                    return TRUE;
+                }
+                else
+                {
+                    if(id > 0)
+                    {
+                        return TRUE;
+                    }
+                    return FALSE;
+                }
+            }
+            static BOOL Enteralimit_student_Main_Subject(const void* data, const void* Limit_parameters)
+            {
+                if(*(grades_type*)data > *(grades_type*)0 && *(grades_type*)data <= *(grades_type*)Limit_parameters)
+                {
+                    return TRUE;
+                }
+                else
+                {
+                    std::cout << "错误!,成绩应在0~" << *(grades_type*)Limit_parameters << "之间" << std::endl;
+                    return FALSE;
+                }
+            }
+        };
+        // student_Grade_checks Check_the_examples;
         ~student_list()
         {
             delete data;
+            prev = nullptr;
+            next = nullptr;
+        }
+        student_list(const student_list& student_list_volume)
+        {
+            data = new student(*(student_list_volume.data));
+            prev = student_list_volume.prev;
+            next = student_list_volume.next;
+        }
+        student_list()
+        {
+            data = new student;
+            prev = nullptr;
+            next = nullptr;
+        }
+        student_list& operator=(const student_list& student_list_volume)
+        {
+            if(this == &student_list_volume)
+            {
+                return *this;
+            }
+            delete data;
+            data = new student(*(student_list_volume.data));
+            prev = student_list_volume.prev;
+            next = student_list_volume.next;
+            return *this;
+        }
+        student_list* student_list_New_Node()
+        {
+            student_list* new_node = new student_list;
+            new_node->data = new student;
+            new_node->prev = nullptr;
+            new_node->next = nullptr;
+            return new_node;
+        }
+        template <typename T>
+        BOOL student_list_stuinputS(std::string Promptwords, T& Input_variable)
+        {
+            while (true)
+            {
+                std::cout << Promptwords << std::endl;
+                std::cin >> Input_variable;
+                if (std::cin.eof())
+                {
+                    std::cout << "输入已结束。" << std::endl;
+                    return FALSE;
+                }
+                std::cin.clear();
+                return TRUE;
+            }
+        }
+        // BOOL student_list_stuinputS(std::string Promptwords, void* Input_variable)
+        // {
+        //     while (true)
+        //     {
+        //         std::cout << Promptwords << std::endl;
+        //         std::cin >> Input_variable;
+        //         if (std::cin.eof())
+        //         {
+        //             std::cout << "输入已结束。" << std::endl;
+        //             return FALSE;
+        //         }
+        //         std::cin.clear();
+        //         return TRUE;
+        //     }
+        // }
+        void student_list_stuinput(student* Input_variables)
+        {
+            //输入检查
+            student_Validation_Limitations::grades <grades_type>  grades_liat;
+            typename Information_management::student_list::student_Grade_checks::inputs verify[] =
+            {
+                {"学号", &(Input_variables->num),              nullptr                                                 ,nullptr},
+                {"姓名", &(Input_variables->name),             nullptr                                                 ,nullptr},
+                {"年龄", &(Input_variables->age),              &student_Validation_Limitations::age_limitation         ,student_list::student_Grade_checks::Enteralimit_student_age},
+                {"性别", &(Input_variables->sex),              nullptr                                                 ,nullptr},
+                {"电话", &(Input_variables->tel),              &student_Validation_Limitations::tel_long               ,student_list::student_Grade_checks::Enteralimit_student_tel},
+                {"地址", &(Input_variables->address),          nullptr                                                 ,nullptr},
+                {"证号", &(Input_variables->identity_card),    &student_Validation_Limitations::address_long           ,student_list::student_Grade_checks::Enteralimit_student_id},
+                {"语文", &(Input_variables->grades.Chinese),   &(grades_liat.grades_main)                              ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"数学", &(Input_variables->grades.Math),      &(grades_liat.grades_main)                              ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"英语", &(Input_variables->grades.English),   &(grades_liat.grades_main)                              ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"政治", &(Input_variables->grades.politics),  &(grades_liat.grades_main_s)                            ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"历史", &(Input_variables->grades.history),   &(grades_liat.grades_main_s)                            ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"地理", &(Input_variables->grades.geography), &(grades_liat.grades_min_s)                             ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"生物", &(Input_variables->grades.biology),   &(grades_liat.grades_min_s)                             ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"化学", &(Input_variables->grades.chemistry), &(grades_liat.grades_min_s)                             ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+                {"物理", &(Input_variables->grades.physics),   &(grades_liat.grades_main_s)                            ,student_list::student_Grade_checks::Enteralimit_student_Main_Subject},
+            };
+            for (uint8_t i = 0; i < sizeof(verify) / sizeof(verify[0]); i++)
+            {
+                while (true)
+                {
+                    if(student_list_stuinputS(verify[i].informationcheck_name, verify[i].address))
+                    {
+                        if(verify[i].Enteralimit != nullptr)
+                        {
+                            if(verify[i].Enteralimit(verify[i].address, verify[i]. Limit_parameters) == false)
+                            {
+                                std::cout << "输入失败" << std::endl;
+                                continue;
+                            }
+                        }
+                        break;
+                    }
+                }
+                
+            }
         }
     };
     public:
@@ -81,6 +319,34 @@ class Information_management
     {
         head = nullptr;
     }
+    
+    // Information_management(const Information_management& Information_management_volume)
+    // {
+    //     head = nullptr;
+    //     student_list* current = Information_management_volume.head->student_list_New_Node();
+    //     current = Information_management_volume.head;
+    //     student_list* prevNode = nullptr;
+    //     while (current != nullptr)
+    //     {
+    //         student_list* newNode = Information_management_volume.head->student_list_New_Node();
+    //         //创建新节点
+    //         newNode->data = current->data;
+    //         newNode->prev = prevNode;
+    //         newNode->next = nullptr;
+    //         if(prevNode != nullptr)
+    //         {
+    //             prevNode->next = newNode;
+    //             //如果上一节点不为空，则将新节点连接到上一节点的下节点
+    //         }
+    //         else
+    //         {
+    //             head = newNode;
+    //             //将新链表连接到this本地头节点
+    //         }
+    //         prevNode = newNode;
+    //         current = current->next;
+    //     }
+    // }
     Information_management(const Information_management& Information_management_volume)
     {
         head = nullptr;
@@ -88,17 +354,16 @@ class Information_management
         student_list* prevNode = nullptr;
         while (current != nullptr)
         {
-            student_list* newNode = new student_list;
-            newNode->data = new student(*(current->data));
+            student_list* newNode = new student_list(*current); // 直接通过拷贝构造函数创建新节点
             newNode->prev = prevNode;
             newNode->next = nullptr;
-            if (prevNode != nullptr)
+            if (prevNode == nullptr)
             {
-                prevNode->next = newNode;
+                head = newNode;
             }
             else
             {
-                head = newNode;
+                prevNode->next = newNode;
             }
             prevNode = newNode;
             current = current->next;
@@ -122,33 +387,29 @@ class Information_management
             return *this;
         }
         //拷贝整个链表
-        student_list* current = head;
-        while (current != nullptr)
-        {
-            student_list* nextNode = current->next;
-            delete current;
-            current = nextNode;
-        }
-        head = nullptr;
-        // 复制新链表
-        current = Information_management_volume.head;
+        student_list* student_list_Deep_copy = Information_management_volume.head->student_list_New_Node();
+        student_list_Deep_copy = Information_management_volume.head;
         student_list* prevNode = nullptr;
-        while (current != nullptr)
+        while (student_list_Deep_copy != nullptr)
         {
-            student_list* newNode = new student_list;
-            newNode->data = new student(*(current->data));
+            student_list* newNode = Information_management_volume.head->student_list_New_Node();
+            //创建新节点
+            newNode->data = student_list_Deep_copy->data;
             newNode->prev = prevNode;
-            newNode->next = nullptr;
-            if (prevNode != nullptr)
+            newNode->next = nullptr;   /*拷贝节点数据*/
+
+            if(prevNode != nullptr)
             {
                 prevNode->next = newNode;
+                //如果上一节点不为空，则将新节点连接到上一节点的下一节点
             }
             else
             {
                 head = newNode;
+                //将新链表连接到this本地头节点
             }
             prevNode = newNode;
-            current = current->next;
+            student_list_Deep_copy = student_list_Deep_copy->next;
         }
         return *this;
     }
@@ -164,14 +425,43 @@ class Information_management
     }
     void Information_management_print(const student_list* cour )  
     {
-        cour->data->Information_management_print();
+        cour->data->student_printf(); 
     }
-        
+
+    void Information_management_Linked_list_Wchecks(student_list*& push_back_Node )
+    {
+        if(push_back_Node == nullptr)
+        {
+           std::cout << "链表为空" << std::endl;
+           std::cout << "请创建链表" << std::endl;
+        }
+    }
+    void Information_management_head_push_back()
+    {
+        student_list* newNode = new student_list();
+        newNode->student_list_stuinput(newNode->data);
+        if (!head)
+        {
+            head = newNode;
+        }
+        else
+        {
+            student_list* current = head;
+            while (current->next)
+            {
+                 current = current->next;
+            }
+            current->next = newNode;
+            newNode->prev = current;
+        }
+    }
+   
 };
-    
+   
 
 int main()
 {
     Information_management < int > Information_management_volume;
+    Information_management_volume.Information_management_head_push_back();
     return 0;
 }
