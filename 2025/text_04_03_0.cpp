bc4fb1809ffd8c225ef7e74e634ffb10123287ed
wangz <outlook.com>
2025-04-06 13:33:58 +0800

    2025年4.6号：提交修改模拟string类

--- 2025/text_04_03_0.cpp
+++ 2025/text_04_03_0.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstring>
 //模拟string
-namespace string_wanz
+namespace wz
 {
     class string
     {
@@ -13,7 +13,7 @@ namespace string_wanz
             //创建迭代器
             typedef char* iterator;
             //限定字符串最大值
-            static const size_t nops = -1;
+            // static const size_t nops = -1;
             iterator begin() 
             {
                 return _data;
@@ -78,11 +78,223 @@ namespace string_wanz
                 }
                 return *this;
             }
+            void operator+=(const string& data_str)
+            {
+                size_t len = _size + data_str._size;
+                if(Automatic_scaling(len) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return;
+                }
+                strcpy(_data + _size,data_str._data);
+                _size = _size + data_str._size;
+                _data[_size] = '\0';
+            }
+            char& operator[](const size_t& ergodic_value)
+            {
+                return _data[ergodic_value];
+            }
+            string operator+(const string& cpp_str_)
+            {
+                string _str_temp;
+                size_t _str_temp_len = _size + cpp_str_._size;
+                if(_str_temp.Automatic_scaling(_str_temp_len) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return _str_temp;
+                }
+                strcpy(_str_temp._data , _data);
+                strcpy(_str_temp._data + _size , cpp_str_._data);
+                _str_temp._size = _size + cpp_str_._size;
+                _str_temp._data[_str_temp._size] = '\0';
+                return _str_temp;
+            }
+            void conversions_oldest()
+            {
+                //字符串转大写
+                for(size_t i = 0; i < _size; ++i)
+                {
+                    if(_data[i] >= 'a' && _data[i] <= 'z')
+                    {
+                        _data[i] -= 32;
+                    }
+                }
+            }
+            void conversions_few()
+            {
+                //字符串转小写
+                for(size_t i = 0; i < _size; ++i)
+                {
+                    if(_data[i] >= 'A' && _data[i] <= 'Z')
+                    {
+                        _data[i] += 32;
+                    }
+                }
+            }
+            // size_t str_substring_kmp(const char*& c_str_substring)
+            // {
+            //     //查找子串
+            // }
+            void nose_Insertion_substrings(const char*& c_str_substring)
+            {
+                //前部插入子串
+                size_t len = strlen(c_str_substring);
+                size_t new_nose_insert_substrings = _size + len;
+                if(Automatic_scaling(new_nose_insert_substrings) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return;
+                }
+                char* _c_nose_insert_substrings_temp = new char[new_nose_insert_substrings + 1];
+                //临时变量
+                strcpy(_c_nose_insert_substrings_temp,_data);
+                strcpy(_data , c_str_substring);
+                strcpy(_data + len , _c_nose_insert_substrings_temp);
+                _size = new_nose_insert_substrings;
+                _data[_size] = '\0';
+                delete [] _c_nose_insert_substrings_temp;
+            }
+            void interlocutory_Insertion_substrings(const char*& c_str_substring,size_t& oid_pos)
+            {
+                //中间位置插入子串
+                size_t len = strlen(c_str_substring);
+                size_t new_interlocutory_insert_substrings = _size + len;
+                if(Automatic_scaling(new_interlocutory_insert_substrings) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return;
+                }
+                char* _c_interlocutory_insert_substrings_temp = new char[new_interlocutory_insert_substrings + 1];
+                //临时变量
+                strcpy(_c_interlocutory_insert_substrings_temp,_data);
+                //从oid_pos开始插入
+                strcpy(_data + oid_pos, c_str_substring);
+                strcpy(_data + oid_pos + len , _c_interlocutory_insert_substrings_temp + oid_pos);
+                _size = new_interlocutory_insert_substrings;
+                _data[_size] = '\0';
+                delete [] _c_interlocutory_insert_substrings_temp;
+            }
+            string str_withdraw(size_t& old_pos)
+            {
+                //提取字串到'\0'
+                string _str_withdraw_temp;
+                size_t _str_withdraw_temp_len = _size - old_pos;
+                if(_str_withdraw_temp.Automatic_scaling(_str_withdraw_temp_len) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return _str_withdraw_temp;
+                }
+                strcpy(_str_withdraw_temp._data , _data + old_pos);
+                _str_withdraw_temp._size = _str_withdraw_temp_len;
+                _str_withdraw_temp._data[_str_withdraw_temp._size] = '\0';
+                return _str_withdraw_temp;
+            }
+            string str_withdraw_extremity(size_t& old_begin)
+            {
+                //提取字串到末尾
+                string _str_withdraw_extremity_temp;
+                size_t _str_withdraw_extremity_temp_len = _size - old_begin;
+                if(_str_withdraw_extremity_temp.Automatic_scaling(_str_withdraw_extremity_temp_len) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return _str_withdraw_extremity_temp;
+                }
+                strcpy(_str_withdraw_extremity_temp._data , _data + old_begin);
+                _str_withdraw_extremity_temp._size = _str_withdraw_extremity_temp_len;
+                _str_withdraw_extremity_temp._data[_str_withdraw_extremity_temp._size] = '\0';
+                return _str_withdraw_extremity_temp;
+            }
+            string str_withdraw_detail(size_t& old_begin ,size_t& old_end)
+            {
+                //提取字串到指定位置
+                string _str_withdraw_detail_temp;
+                size_t _str_withdraw_detail_temp_len = old_end - old_begin;
+                if(_str_withdraw_detail_temp.Automatic_scaling(_str_withdraw_detail_temp_len) != true)
+                {
+                    std::cout << "开辟内存失败！" << std::endl;
+                    return _str_withdraw_detail_temp;
+                }
+                strcpy(_str_withdraw_detail_temp._data , _data + old_begin);
+                _str_withdraw_detail_temp._size = _str_withdraw_detail_temp_len;
+                _str_withdraw_detail_temp._data[_str_withdraw_detail_temp._size] = '\0';
+                return _str_withdraw_detail_temp;
+            }
+            bool Automatic_scaling(size_t& temporary_variable)
+            {
+                //检查string空间大小，来分配内存
+                char* temporary_ = new char[temporary_variable+1];
+                if(temporary_)
+                {
+                    strcpy(temporary_,_data);
+                    delete[] _data;
+                    _data = temporary_;
+                    _capacity = temporary_variable;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            void push_back(const char& c_temp_str)
+            {
+                if(_size == _capacity)
+                {
+                    size_t newcapacity = _capacity == 0 ? 2 :_capacity*2;
+                    if(Automatic_scaling(newcapacity) != true)
+                    {
+                        std::cout << "开辟内存失败！" << std::endl;
+                        return;
+                    }
+                }
+                _data[_size] = c_temp_str;
+                ++_size;
+                _data[_size] = '\0';
+            }
+            void push_back(const string& cpp_temp_str)
+            {
+                size_t len = _size + cpp_temp_str._size;
+                if(len > _capacity)
+                {
+                    size_t new_capacity = len;
+                    if(Automatic_scaling(new_capacity) != true)
+                    {
+                        std::cout << "开辟内存失败！" << std::endl;
+                        return;
+                    }
+                }
+                strcpy(_data+_size,cpp_temp_str._data);
+                _size =_size + cpp_temp_str._size;
+                _data[_size] = '\0';
+            }
+            void push_back(const char*& c_temp_str)
+            {
+                size_t len = strlen( c_temp_str );
+                size_t new_capacity = len + _size ;
+                if(new_capacity >_capacity)
+                {
+                    if(Automatic_scaling( new_capacity) != true)
+                    {
+                        std::cout << "开辟内存失败！" << std::endl;
+                        return;
+                    }
+                }
+                strcpy(_data+_size , c_temp_str);
+                _size = _size + len;
+                _data[_size] = '\0';
+            }
+    };
+    class string_C_return
+    {
+        public:
+            typedef char Temporary_needs;
+
+        
     };
     std::ostream& operator<<(std::ostream& string_ostream,string &data_str) 
     {
         //当前没实现【】访问.可以用迭代器
-        for(string_wanz::string::iterator originate = data_str.begin();originate != data_str.end();originate++)
+        for(wz::string::iterator originate = data_str.begin();originate != data_str.end();originate++)
         {
             string_ostream << *originate;
         }
@@ -92,9 +304,16 @@ namespace string_wanz
 
 int main()
 {
-    string_wanz::string s1;
-    string_wanz::string s2("hello");
-    std::cout << s1 << std::endl;
-    std::cout << s2 << std::endl;
+    wz::string str1("hello");
+    wz::string str2("world");
+    wz::string str3 = str1 + str2;
+    std::cout << "str3: " << str3 << std::endl;
+    str3.push_back('!');
+    const char* insert_str = "inserted";
+    str3.nose_Insertion_substrings(insert_str);
+    std::cout << "str3 after insertion: " << str3 << std::endl;
+    size_t old_pos = strlen(insert_str);
+    wz::string str4 = str3.str_withdraw(old_pos);
+    std::cout << "str4: " << str4 << std::endl;
     return 0;
 }
