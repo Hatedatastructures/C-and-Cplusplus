0a509d21c540ecf3e4e8336dfd18daa023b7d7f4
Hatedatastructurse <c13738349304@outlook.com>
2025-04-10 13:26:20 +0800

    vector接口使用

--- 2025/text_04_09_0.cpp
+++ 2025/text_04_09_0.cpp
@@ -5,11 +5,11 @@
 int main()
 {
     std::vector <int> test;
-    test.push_back(1);
-    test.push_back(2);
-    test.push_back(3);
-    test.push_back(4);
     test.push_back(5);
+    test.push_back(4);
+    test.push_back(3);
+    test.push_back(2);
+    test.push_back(1);
 
     for(std::vector <int> ::iterator i = test.begin();i != test.end();i++)
     {
@@ -21,6 +21,26 @@ int main()
     for(std::vector <int> ::reverse_iterator i = test.rbegin();i != test.rend();i++)
     {
         //反向迭代器
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+    std::sort(test.begin(),test.end());
+    for(std::vector <int> ::iterator i = test.begin();i != test.end();i++)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+
+    std::sort(test.rbegin(),test.rend());
+    for(std::vector <int> ::iterator i = test.begin();i != test.end();i++)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+
+    std::reverse(test.begin(),test.end());
+    for(std::vector <int> ::iterator i = test.begin();i != test.end();i++)
+    {
         std::cout << *i << " ";
     }
     std::cout << std::endl;
