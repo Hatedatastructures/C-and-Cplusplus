fe553813d41469a715b5553b2b80a6fab097e795
Hatedatastructurse <c13738349304@outlook.com>
2025-04-13 00:15:25 +0800

    未实现函数接口

--- 2025/text_04_11_0.cpp
+++ 2025/text_04_11_0.cpp
@@ -47,7 +47,11 @@ namespace wang
                     _data_pointer[i] = data;
                 }
             }
-            
+            //问题：构造函数问题，拷贝构造函数逻辑问题 ，运算符+重载问题 ，运算符<<重载问题
+            //需要实现：resize 函数 扩大 vector_t 个 内存
+            //返回头数据 ，返回尾数据 ，查找数据
+            //填充数据函数
+            //重载operator++() 返回迭代器，数据起始位置
             vector(const vector<vector_t>& temp_data)
             {
                 vector <vector_t> temp_data_copy(temp_data._size_pointer - temp_data._data_pointer , *temp_data._data_pointer);
@@ -73,13 +77,17 @@ namespace wang
                 while (temp != _size_pointer)
                 {
                     //(temp-1)就是pos的位置，从pos位置开始覆盖，覆盖到倒数第1个结束，最后一个会被--屏蔽掉
-                    *(temp-1) = temp;
+                    *(temp-1) = *temp;
                     temp++;
                 }
                 --_size_pointer;
                 return pos;
             }
-            friend std::ostream& operator<<(std::ostream& vector_ostream,vector<vector_t>& Dynamic_arrays_data);
+            vector& resize(const size_t& reszie_szie_)
+            {
+                vector<vector_t> reszie_temp = reszie_szie_;
+            }
+            friend std::ostream& operator<< (std::ostream& vector_ostream,const vector<vector_t>& Dynamic_arrays_data);
             vector_t& operator[](const size_t& _size_operator)
             {
                 return _data_pointer[_size_operator];
@@ -88,14 +96,20 @@ namespace wang
             {
                 return _data_pointer[_size_operator];
             }
+            vector_t& operator=(const vector<vector_t>&vector_temp_)
+            {
+                vector<vector> _temp_(vector_temp_.size_v(),*vector_temp_._data_pointer);
+                //
+            }
     };
     template <typename vector_t>
-    std::ostream& operator<<(std::ostream& vector_ostream, vector<vector_t>& Dynamic_arrays_data)
+    std::ostream& operator<<(std::ostream& vector_ostream, const vector<vector_t>& Dynamic_arrays_data)
     {
-        for(typename vector<vector_t>::const_iterator Output_iterators = Dynamic_arrays_data.begin();Output_iterators != Dynamic_arrays_data.end();Output_iterators++)
+        for(int i = 0; i < Dynamic_arrays_data.size_v(); i++)
         {
-            vector_ostream << *Output_iterators;
+            vector_ostream << Dynamic_arrays_data[i];
         }
+        vector_ostream << std::endl;
     }
 }
 int main()
