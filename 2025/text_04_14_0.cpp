1654d7f9fed3928f05296c91c86720d39a213149
Hatedatastructurse <c13738349304@outlook.com>
2025-04-14 18:37:02 +0800

    实现一个简单的list容器能遍历

--- 2025/text_04_14_0.cpp
+++ 2025/text_04_14_0.cpp
@@ -5,23 +5,24 @@ namespace wa
     template<typename list_Ndoe_Type>
     struct listNode
     {
-        list_Ndoe_Type _data;
         listNode<list_Ndoe_Type>* _prev;
         listNode<list_Ndoe_Type>* _next;
+        list_Ndoe_Type _data;
 
         listNode(const list_Ndoe_Type& data = list_Ndoe_Type())
         :_prev(nullptr), _next(nullptr), _data(data)
         {
             //列表初始化
         }
+        
     };
     template <typename list_Node_Type>
     class _list_iterator_
     {
+    public:
         //迭代器类
         typedef listNode<list_Node_Type> Node;
         Node* _node;
-    public:
         _list_iterator_(Node* node)
         :_node(node)
         {
@@ -34,9 +35,21 @@ namespace wa
         }
         _list_iterator_<list_Node_Type>& operator++()
         {
-            //返回加之后的位置
+            //先用再加，返回加之后的位置
             _node = _node -> _next;
             return *this;
+        }
+        _list_iterator_<list_Node_Type> operator++(int)
+        {
+            //先加再用返回加之前的位置
+            _list_iterator_<list_Node_Type> temp = *this;
+            ++(*this);
+            return temp;
+        }
+        bool operator!= (const _list_iterator_<list_Node_Type>& _iterator_temp_)
+        {
+            //比较两个指针及其上一个和下一个指针地址
+            return _node != _iterator_temp_._node;
         }
     };
     template <typename list_Node_Type>
@@ -45,6 +58,7 @@ namespace wa
         typedef listNode<list_Node_Type> Node;
 
         Node* _head;
+        //_head为哨兵位
         void CreateHead()
         {
             _head = new Node;
@@ -52,19 +66,45 @@ namespace wa
             _head -> _next = _head;
         }
     public:
+        typedef _list_iterator_<list_Node_Type> iterator;
         list()
         {
             CreateHead();
+        }
+        iterator begin()
+        {
+            //因为_head为哨兵位，所以哨兵下一个结点为有效数据
+            return iterator(_head ->_next);
+        }
+        iterator end()
+        {
+            return iterator(_head);
         }
         void push_back(const list_Node_Type& push_back_data)
         {
             Node* tail = _head->_prev;
             Node* new_node = new Node(push_back_data); 
             //开辟内存拷贝把list_Node_Type类型赋值到_data里
-            tail->next = new_node;
+            tail->_next = new_node;
             new_node->_prev = tail;
             _head->_prev = new_node;
             new_node->_next = _head;
         }
     };
+}
+int main()
+{
+    wa::list<int> test1;
+    for(size_t i = 1; i < 10; i++)
+    {
+        test1.push_back(i);
+    }
+    wa::list<int>::iterator it = test1.begin();
+    while(it != test1.end())
+    {
+        std::cout << *it  << " ";
+        it++;
+    }
+    std::cout << std::endl;
+    return 0;
 }
