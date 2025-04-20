d4877a581cecbdf05184aad8c3e984bc932981cf
Hatedatastructurse <c13738349304@outlook.com>
2025-04-20 15:13:04 +0800

    添加继承例子更利于理解

--- 2025/text_04_20_0.cpp
+++ 2025/text_04_20_0.cpp
@@ -16,9 +16,48 @@ class B :public A
         // std::cout << ::A::sum << std::endl;
     }
 };
+
+
+
+/*                对象演示              */
+class Information
+{
+    public:
+    std::string name;
+    size_t tel;
+    size_t ID;
+};
+// 定义一个名为Student的类，该类继承自Information类
+class Student : public Information
+{
+    // 私有成员变量，只能在类的内部访问
+    private:
+        // std::string类型的成员变量，用于存储学生的班级信息
+    std::string Class;
+        // size_t类型的成员变量，用于存储学生的成绩，size_t通常用于表示大小或计数
+    size_t grades;
+};
+
+class teacher : public Information
+{
+    private:
+    size_t Job_number;
+    bool director;
+    std::string category;
+};
+
+class Dormitory_management:public Information
+{
+    private:
+    size_t Job_number;
+}
+
+
 int main()
 {
     B test;
     test.printf();
+
+
     return 0;
 }
