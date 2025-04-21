5ae57e7d975a0f9c02122ae8c406c342b00f712f
Hatedatastructurse <c13738349304@outlook.com>
2025-04-21 17:42:52 +0800

    类继承区别：访问限定符来保证访问哪些区域的数据，修改上一次提交的类名错误

--- 2025/text_04_21_0.cpp
+++ 2025/text_04_21_0.cpp
@@ -20,6 +20,13 @@ class Student : public Person
 {
     protected :
         int _stuNum ; // 学号
+    public:
+        void fucn()
+        {
+            std::cout << ::Person::_count << std::endl;
+        }
+    private:
+        size_t szie;
 };
 class Graduate : public Student
 {
@@ -28,11 +35,14 @@ protected:
 };
 int main()
 {
+    //通过访问限定符来保持
     Student s1;
     Student s2;
     Student s3;
     Student s4;
-    greater g2;
+    Graduate g2;
+    g2.fucn();
+    cout << g2._count << endl;
     cout << Person::_count << endl;
     return 0;
 }
