47861caeb5b9997c0eaabd4787dfe9613a50cf40
Hatedatastructurse <c13738349304@outlook.com>
2025-06-16 15:41:35 +0800

    添加信息类和文件检测

--- template_containers/custom_logs.hpp
+++ template_containers/custom_logs.hpp
@@ -1,43 +1,187 @@
 #pragma once
 #include "template_container.hpp"
 #include <fstream>
-namespace file_log
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+using custom_string = con::string;
+using log_timestamp_class = std::chrono::system_clock;  //时间戳类
+namespace custom_log
 {
-    class log
+    [[nodiscard]] bool file_exists(const std::string& path)
+    {
+        std::ifstream status_judgment(path);
+        return status_judgment.good();
+    }
+    [[nodiscard]] bool file_exists(const custom_string& path)
+    {
+        std::ifstream status_judgment(path.c_str());
+        return status_judgment.good();
+    }
+    [[nodiscard]] bool file_exists(const char* path)
+    {
+        std::ifstream status_judgment(path);
+        return status_judgment.good();
+    }
+    enum security_level
+    {
+        
+    };
+    class file_foundation_log
+    {
+    public:
+        using inbuilt_documents = std::ofstream;
+        inbuilt_documents file_ofstream;
+        file_foundation_log() = default;
+        file_foundation_log(const file_foundation_log& rhs) = delete;
+        file_foundation_log(file_foundation_log&& rhs) = delete;
+        file_foundation_log& operator=(const file_foundation_log& rhs) = delete;
+        file_foundation_log& operator=(file_foundation_log&& rhs) = delete;
+        ~file_foundation_log()
+        {
+            file_ofstream.close();
+        }
+        file_foundation_log(const custom_string& file_name)
+        {
+            file_ofstream.open(file_name.c_str());
+        }
+        file_foundation_log(const char* file_name)
+        {
+            file_ofstream.open(file_name);
+        }
+        file_foundation_log(const std::string& file_name)
+        {
+            file_ofstream.open(file_name.c_str());
+        }
+    };
+    class information
+    {
+    public:
+        custom_string debugging_information;        //调试信息
+        custom_string general_information;          //一般信息
+        custom_string warning_messages;             //警告信息
+        custom_string error_messages;               //错误信息
+        custom_string critical_misinformations;     //严重错误信息
+        information() = default;
+        information(const information& rhs) = delete;
+        information(information&& rhs) = delete;
+        information& operator=(const information& rhs) = delete;
+        information& operator=(information&& rhs) = delete;
+        ~information() = default;
+        void debugging_information_input(const custom_string& information_data)
+        {
+            debugging_information = information_data;
+        }
+        void debugging_information_input(const std::string& information_data)
+        {
+            debugging_information = information_data.c_str();
+        }
+        void debugging_information_input(const char* information_data)
+        {
+            debugging_information = information_data;
+        }
+        void general_information_input(const custom_string& information_data)
+        {
+            general_information = information_data;
+        }
+        void general_information_input(const std::string& information_data)
+        {
+            general_information = information_data.c_str();
+        }
+        void general_information_input(const char* information_data)
+        {
+            general_information = information_data;
+        }
+        void warning_messages_input(const custom_string& information_data)
+        {
+            warning_messages = information_data;
+        }
+        void warning_messages_input(const std::string& information_data)
+        {
+            warning_messages = information_data.c_str();
+        }
+        void warning_messages_input(const char* information_data)
+        {
+            warning_messages = information_data;
+        }
+        void error_messages_input(const custom_string& information_data)
+        {
+            error_messages = information_data;
+        }
+        void error_messages_input(const std::string& information_data)
+        {
+            error_messages = information_data.c_str();
+        }
+        void error_messages_input(const char* information_data)
+        {
+            error_messages = information_data;
+        }
+        void critical_misinformations_input(const custom_string& information_data)
+        {
+            critical_misinformations = information_data;
+        }
+        void critical_misinformations_input(const std::string& information_data)
+        {
+            critical_misinformations = information_data.c_str();
+        }
+        void critical_misinformations_input(const char* information_data)
+        {
+            critical_misinformations = information_data;
+        }
+    };
+    class function_stacks
+    {
+        con::stack<con::string> function_log_stacks;
+    public:
+        function_stacks() = default;
+        function_stacks(const function_stacks& rhs) = delete;
+        function_stacks(function_stacks&& rhs) = delete;
+        function_stacks& operator=(const function_stacks& rhs) = delete;
+        function_stacks& operator=(function_stacks&& rhs) = delete;
+        ~function_stacks() = default;
+        void push (const custom_string& function_name)
+        {
+            function_log_stacks.push(function_name);
+        }
+        void push(const std::string& function_name)
+        {
+            function_log_stacks.push(function_name.c_str());
+        }
+        void push(const char* function_name)
+        {
+            function_log_stacks.push(function_name);
+        }
+        con::string top()
+        {
+            return function_log_stacks.top();
+        }
+        size_t size()
+        {
+            return function_log_stacks.size();
+        }
+        bool empty()
+        {
+            return function_log_stacks.empty();
+        }
+        void pop()
+        {
+            function_log_stacks.pop();
+        }
+    };
+    class foundation_log
     {
     private:
-        using log_string = con::string;
-        using log_file   = std::ofstream;
-        log_string file_name;
-        log_file file;          //文件对象
+        std::chrono::_V2::system_clock::time_point foundation_log_timestamp;
     public:
-        log() = default;
-        log(const log& rhs) = delete;
-        log(log&& rhs) = delete;
-        log& operator=(const log& rhs) = delete;
-        log& operator=(log&& rhs) = delete;
-
-        explicit log(const char* str_log)
+        foundation_log() = default;
+        foundation_log(const foundation_log& rhs) = delete;
+        foundation_log(foundation_log&& rhs) = delete;
+        foundation_log& operator=(const foundation_log& rhs) = delete;
+        foundation_log& operator=(foundation_log&& rhs) = delete;
+        ~foundation_log() = default;
+        void timestamp_acquisition(const std::chrono::_V2::system_clock::time_point& timestamp_data) noexcept
         {
-            file_name = str_log;
-            file.open(file_name.c_str());
-        }
-        explicit log(const log_string& customize_string_log)
-        {
-            file_name = customize_string_log;
-            file.open(file_name.c_str());
-        }
-        void wield_file(const char* str_log)
-        {
-            file << str_log << std::endl;
-        }
-        void write_file(const log_string& customize_string_log)
-        {
-            file << customize_string_log << std::endl;
-        }
-        virtual ~log()
-        {
-            file.close();
+            foundation_log_timestamp = timestamp_data;
         }
     };
 }
--- template_containers/log_test/test.cpp
+++ template_containers/log_test/test.cpp
@@ -2,14 +2,32 @@
 #include "../template_container.hpp"
 int main()
 {
-    {
-        file_log::log text_string_log (con::string("日志文件.txt"));
-        const con::string log_string = "This is a log string";
-        std::cout << log_string << std::endl;
-        text_string_log.write_file(log_string);
-        text_string_log.write_file(log_string);
-        text_string_log.wield_file("文件测试");
-        // text_string_log.wield_file(log_string);
-    }
+    // {
+    //     custom_log::foundation_log text_string_log (con::string("日志文件.txt"));
+    //     con::string custom_string = "This is a foundation_log string";
+    //     std::string storehouse_log_string = "This is a storehouse foundation_log string";
+    //     std::cout << custom_string << std::endl;
+    //     text_string_log.write_file(custom_string);
+    //     text_string_log.write_file(custom_string);
+    //     text_string_log.write_file("文件测试");
+    //     text_string_log.write_file(custom_string);
+    //     text_string_log.write_file(storehouse_log_string);
+    // }
+    // auto now = std::chrono::system_clock::now();
+    // std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+    // std::cout << current_time << std::endl;
+    
+    // // 转换为本地时间结构
+    // std::tm* localTime = std::localtime(&current_time);
+    
+    // // 格式化输出
+    // std::cout << "当前时间: " << std::put_time(localTime, "%Y-%m-%d %H:%M:%S") << std::endl;
+    con::string file_name = "日志文件.txt";
+    std::string storehouse_file_name = file_name.c_str();
+    std::cout << custom_log::file_exists("日志文件.txt") << std::endl;
+    std::cout << custom_log::file_exists(file_name) << std::endl;
+    std::cout << custom_log::file_exists(storehouse_file_name) << std::endl;
+    con::string count;
+    std::cout << "输出count:" << count << std::endl;
     return 0;
 }
