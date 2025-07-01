483135c7c2a064b51554ec6910e309fdf432906f
Hatedatastructurse <c13738349304@outlook.com>
2025-07-01 23:14:57 +0800

    添加python学习文件

---
+++ python_learning/text_07_01_0.py
@@ -0,0 +1,11 @@
+print("hello","world","你","好",sep= " H ")
+print(False)
+number = 1
+print(type(number))
+user_name = "你好,这是一个测试信息"
+print( "输出信息:",user_name)
+print("输出信息: %s" % user_name)
+print(f"输出信息: {user_name} {number}")
+str_name = input("请输入你的名字:")
+print(f"你好, {str_name}")
+print("你好, %s" % str_name)
