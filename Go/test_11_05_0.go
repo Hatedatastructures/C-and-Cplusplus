9448702a9486074b7f10f6e43945140d6bc51b6b
Hatedatastructures <c13738349304@outlook.com>
2025-11-05 23:14:49 +0800

    refactor(Go/test_11_05_0.go): 重构测试代码并添加时间处理功能
    移除未使用的html字符串代码，添加returnVlaue函数和time包的使用
    更新.gitignore文件，忽略.mod文件

--- Go/test_11_05_0.go
+++ Go/test_11_05_0.go
@@ -1,29 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // import "net/http"
 
-func test() {
-	string_value := "string type"
-	fmt.Println("string :", string_value)
-	fmt.Printf("string type = %T \n", string_value)
-	// fmt.Println("我是一个函数" + "我会打印函数")
+//	func test() {
+//		string_value := "string type"
+//		fmt.Println("string :", string_value)
+//		fmt.Printf("string type = %T \n", string_value)
+//		// fmt.Println("我是一个函数" + "我会打印函数")
+//	}
+func returnVlaue() (string, int) {
+	return "hello world", 100
 }
 
 func main() {
-	test()
-	var html_string string = `
-	<html>
-		<head>
-			<title>hello world</title>
-		</head>
-		<body>
-			<h1>hello world</h1>
-		</body>
-	</html>
-	`
-	fmt.Println(html_string)
+	// test()
+	// var html_string string = `
+	// <html>
+	// 	<head>
+	// 		<title>hello world</title>
+	// 	</head>
+	// 	<body>
+	// 		<h1>hello world</h1>
+	// 	</body>
+	// </html>
+	// `
+	// fmt.Println(html_string)
 
 	// fmt.Println("hello world")
 	// var a string // 定义一个字符串变量a
@@ -31,4 +37,13 @@ func main() {
 	// fmt.Println("请输入一个字符串")
 	// fmt.Scanf("%s", &a)
 	// fmt.Println("你输入的字符串是：" + a)
+	{
+		var username, _ = returnVlaue() // 忽略第二个返回值
+		var _, age = returnVlaue()      // 忽略第一个返回值
+		fmt.Println(username)
+		fmt.Println(age)
+		t := time.Date(2023, time.Month(11), 5, 10, 0, 0, 0, time.Local)
+		fmt.Println(t.Month())
+		fmt.Println(t)
+	}
 }
